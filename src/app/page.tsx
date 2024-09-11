0a0906eb211ce8caafbb41dd0e332853c7877797
Johnson Hsiung <johnson.hsiung@sjsu.edu>
2024-09-11 14:15:17 -0700

    form to input new todo items

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,24 @@ type ToDoItem = {
 };
 
 export default function Home() {
-  const [todos, setTodos] = useState<ToDoItem[]>([]);
+  const [todos, setTodos] = useState<ToDoItem[]>([
+    {title: "example", description: "example", completed: false}
+  ]);
+
+  const [title, setTitle] = useState<string>("")
+  const [description, setDescription] = useState("")
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault(); 
+    setTodos(prev => {
+      const newTodos = [...prev];
+      newTodos.push({title, description, completed: false});
+      setTitle("");
+      setDescription("");
+      return newTodos;
+    })
+
+  };
   return (
     // md - maximum width of div to medium size
     // mx - margin set to auto
@@ -18,16 +35,30 @@ export default function Home() {
       <ul>
         {todos.map(({ title, description, completed }, index) => (
           <li key={index}>
-      
-            <input type="checkbox" checked={completed} onChange={e => setTodos(prev => { // prev (can be named anything) is the current state of todos before the update 
-              prev[index].completed = e.target.checked;
-              return prev;
-            })} />
+            <input
+              type="checkbox"
+              checked={completed}
+              onChange={(e) =>
+                setTodos((prev) => {
+                  // prev (can be named anything) is the current state of todos before the update
+                  const newTodo = [...prev]
+                  newTodo[index].completed = e.target.checked;
+                  return newTodo;
+                })
+              }
+            />
             <span className="font-semibold">{title}</span>
             {description}
           </li>
         ))}
       </ul>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="title"> Title</label>
+        <input type="text" name="title" id="title" value={title} onChange={e => setTitle(e.target.value)} />
+        <label htmlFor="description"> Description</label>
+        <input type="text" name="description" id="description" value={description} onChange={e => setDescription(e.target.value)}/>
+        <button type="submit">Create</button>
+      </form>
     </div>
   );
 }
