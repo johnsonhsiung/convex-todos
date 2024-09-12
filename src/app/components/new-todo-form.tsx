019c2cabb117ac0804372088c276d8eed8c365b4
Johnson Hsiung <johnson.hsiung@sjsu.edu>
2024-09-12 16:19:15 -0700

    add and remove buttons with styling

--- src/app/components/new-todo-form.tsx
+++ src/app/components/new-todo-form.tsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
 type TodoFormProps = {
   onCreate: (title: string, description: string) => void;
-}
+};
 // props (short for "properties") are a mechanism for passing data from a parent component to a child component
-export function NewTodoForm( {onCreate} : TodoFormProps ) {
+export function NewTodoForm({ onCreate }: TodoFormProps) {
   const [title, setTitle] = useState<string>("");
   const [description, setDescription] = useState("");
 
@@ -15,23 +15,27 @@ export function NewTodoForm( {onCreate} : TodoFormProps ) {
   };
   return (
     <form onSubmit={handleSubmit}>
-      <label htmlFor="title"> Title</label>
-      <input
-        type="text"
-        name="title"
-        id="title"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-      />
-      <label htmlFor="description"> Description</label>
-      <input
-        type="text"
-        name="description"
-        id="description"
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}
-      />
-      <button type="submit">Create</button>
+      <div className="flex flex-col gap-2">
+        <label className="text-sm font-semibold" htmlFor="title" > Title</label>
+        <input
+          className="p1 border rounded"
+          type="text"
+          name="title"
+          id="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label className="text-sm font-semibold" htmlFor="description"> Description</label>
+        <input
+          className="p1 border rounded"
+          type="text"
+          name="description"
+          id="description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <button className="bg-blue-500 p-1 rounded text-white" type="submit">Create</button>
+      </div>
     </form>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,26 +17,34 @@ export default function Home() {
     // md - maximum width of div to medium size
     // mx - margin set to auto
     // padding
-    <div className="max-w-screen-md mx-auto p4">
+    <div className="max-w-screen-md mx-auto p4 space-y-4">
       <h1 className="text-xl font-bold"> TodoList</h1>
-      <ul>
+      <ul className="space-y-2">
         {todos.map(({ title, description, completed }, index) => (
-          <li key={index}>
-            <input
-              type="checkbox"
-              checked={completed}
-              onChange={(e) =>
-                setTodos((prev) => {
-                  // prev (can be named anything) is the current state of todos before the update
-                  const newTodo = [...prev];
-                  newTodo[index].completed = e.target.checked;
-                  return newTodo;
-                })
-              }
-            />
-            <span className="font-semibold">{title}</span>
-            {description}
-          </li>
+          <TodoItem
+            key={index}
+            title={title}
+            description={description}
+            completed={completed}
+            onCompleteChange={(newValue) => {
+              setTodos((prev) => {
+                // prev (can be named anything) is the current state of todos before the update
+                const newTodo = [...prev];
+                newTodo[index].completed = newValue;
+                return newTodo;
+              });
+            }}
+            onRemove={ () => {
+              setTodos((prev) => {
+                // prev (can be named anything) is the current state of todos before the update
+                const newTodo = [...prev].filter((_, i) => i !== index)
+                return newTodo;
+              });
+
+            }
+
+            }
+          />
         ))}
       </ul>
       <NewTodoForm
@@ -49,5 +57,37 @@ export default function Home() {
         }}
       ></NewTodoForm>
     </div>
+  );
+}
+
+function TodoItem({
+  title,
+  description,
+  completed,
+  onCompleteChange,
+  onRemove
+}: {
+  title: string;
+  description: string;
+  completed: boolean;
+  onCompleteChange: (newValue: boolean) => void;
+  onRemove: () => void; 
+}) {
+  return (
+    <li className="flex gap-2 items-center border rounded p-2 w-full">
+      <input
+        type="checkbox"
+        checked={completed}
+        onChange={(e) => onCompleteChange(e.target.checked)}
+      />
+      <div>
+        <p className="font-semibold">{title}</p>
+        <p className="text-sm text-gray-500">{description}</p>
+      </div>
+      <div className="ml-auto" >
+        <button onClick={() => onRemove() }>Remove</button>
+      </div>
+      
+    </li>
   );
 }
